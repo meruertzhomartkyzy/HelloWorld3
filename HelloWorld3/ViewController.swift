90b065d7591e2e8de56cbdfa688a99ab9f159fb2
Meruert <meruertkabylbek@gmail.com>
2024-02-21 12:17:53 +0600

    Add hide/show textlogic

--- HelloWorld3/ViewController.swift
+++ HelloWorld3/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var HelloWorldLabel: UILabel!
+    
+    @IBOutlet var toggleButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        HelloWorldLabel.isHidden = true
+        toggleButton.layer.cornerRadius = 10
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func makeButtonAction() {
+        if HelloWorldLabel.isHidden {
+            HelloWorldLabel.isHidden = false
+            toggleButton.setTitle("Hide Text", for: .normal)
+        } else {
+            HelloWorldLabel.isHidden = true
+            toggleButton.setTitle("Show Text", for: .normal)
+        }
+    }
+    
 }
 
